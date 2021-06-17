09dfdea4da4c12dfa76087ed0a86388fe131b5da
Turao <lenz.arthur@gmail.com>
2021-06-17 15:41:20 -0300

    Add skeleton files

---
+++ src/test/java/com/n26/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.n26;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+  @Test
+  public void contextLoads() {}
+}
